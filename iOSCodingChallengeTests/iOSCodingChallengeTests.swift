2b7e398a26841a15ad4bdaa7d0b982e48effd3c7
Shaggy13spe <shaggy13spe@gmail.com>
2026-01-07 16:57:07 -0500

    Initial Commit

---
+++ iOSCodingChallengeTests/iOSCodingChallengeTests.swift
@@ -0,0 +1,16 @@
+//
+//  iOSCodingChallengeTests.swift
+//  iOSCodingChallengeTests
+//
+//  Created by Michael Morrison on 1/7/26.
+//
+
+import Testing
+
+struct iOSCodingChallengeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
